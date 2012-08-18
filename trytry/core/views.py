04c3c7095415b9db0ab7feebd7858cab396e00ae
Vavilin <shtartora@gmail.com>
2012-08-18 19:05:37 +0600

    Get index.html or redirect to some flow

--- trytry/core/views.py
+++ trytry/core/views.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from trytry.core.utils import get_all_flows
 
 
 def index(request):
+    flow = request.GET.get('flow', None)
+    if flow and flow in get_all_flows().keys():
+        view = "trytry.%(flow)s.views.%(flow)s_get_task" % {'flow': flow}
+        return HttpResponseRedirect(reverse(view))
     return render(request, 'core/index.html', {})
