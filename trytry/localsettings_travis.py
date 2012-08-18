a1ee538a90d8bb1db8caa35bae70a0f060f040c9
Roman Imankulov <roman.imankulov@gmail.com>
2012-08-18 14:34:54 +0600

    Added localsettings for Travis-CI

---
+++ trytry/localsettings_travis.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import os
+
+BASEDIR = os.path.dirname(os.path.realpath(__file__))
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Roman Imankulov', 'roman.imankulov@gmail.com'),
+    ('Pavel Vavilin', 'shtartora@gmail.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASEDIR, 'sqlite.db'),
+    }
+}
+SECRET_KEY = 'PSb3lIO8SIpHsIlq4axbxYRl1XPO6WsKuHlVur32'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    }
+}
