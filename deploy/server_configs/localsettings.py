7c8a22d5f26fc6a78ab784b603dbdef54aafbcfb
Roman Imankulov <roman.imankulov@gmail.com>
2012-08-18 19:13:37 +0600

    Added localsettings for deployment

---
+++ deploy/server_configs/localsettings.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import os
+
+BASEDIR = os.path.dirname(os.path.realpath(__file__))
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Roman Imankulov', 'roman.imankulov@gmail.com'),
+    ('Pavel Vavilin', 'shtartora@gmail.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASEDIR, 'sqlite.db'),
+    }
+}
+SECRET_KEY = 'z#@+2a=i&amp;dgckelr!sy340dl@&amp;&amp;j0k)^tny$=nb#%#8-jfztys'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
