710e6a11c932b5aa4076a01f055a3a5f707c6763
Vavilin <shtartora@gmail.com>
2012-08-18 20:13:41 +0600

    Nice interface for trytry project

--- trytry/core/views.py
+++ trytry/core/views.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
-from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
 from trytry.core.utils import get_all_flows
 
 
 def index(request):
     flow = request.GET.get('flow', None)
     all_flows = get_all_flows().keys()
+    flow_name = None
     if flow and flow in all_flows:
-        view = "trytry.%(flow)s.views.%(flow)s_get_task" % {'flow': flow}
-        return HttpResponseRedirect(reverse(view))
-    return render(request, 'core/index.html', {'all_flows': all_flows})
+        flow_name = "trytry.{0}.views".format(flow)
+    return render(request, 'core/index.html', {'all_flows': all_flows, 'flow_name': flow_name})
