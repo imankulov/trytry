b9abd15338414c6776ee29a8ca7887e0965df83d
Vavilin <shtartora@gmail.com>
2012-08-19 20:53:33 +0600

    Multiline support

--- trytry/project_static/js/main.js
+++ trytry/project_static/js/main.js
@@ -1,24 +1,59 @@
-var updateStep, updateData, return_prompt,
-    url = '/' + flow_url + '/';
+var updateStep, updateData, prompt,
+        url = '/' + flow_url + '/',
+        multilineMode = false,
+        multilineEnd = false,
+        commandArray = [];
+
 
 $(function () {
+    // Now i understand, that i don't understand JavaScript!
     $.getJSON(url, function (data) {
         step_prompt = data.step_prompt;
+        prompt = step_prompt;
         updateData(data);
         var terminal = $("#code-block").terminal(function (command, term) {
-                var regexp = /^\s*$/;
-                if (!regexp.test(command)) {
-                    term.pause();
-                    $.post(url, {'command': command}, function (data) {
-                        if (data.err_text) {
-                            term.error(data.err_text);
-                        } else  if (data.ok_text) {
-                            term.echo("[[i;#E07B08;#000]" + data.ok_text + "]");
+                var regexp = /^\s*$/,
+                    regexp_test = regexp.test(command);
+                if (command === 'multiline on') {
+                    multilineMode = true;
+                    prompt = '[' + step_prompt + '] ';
+                    term.set_prompt(prompt);
+                    commandArray = [];
+                } else if (command === 'multiline off' && !commandArray.length) {
+                    multilineMode = false;
+                    prompt = step_prompt;
+                    term.set_prompt(prompt);
+                    commandArray = [];
+                } else if (!regexp_test || multilineEnd) {
+                    if (!multilineMode || (multilineEnd && regexp_test)) {
+                        term.pause();
+                        commandArray.push(command);
+                        command = commandArray.join('\n');
+                        $.post(url, {'command': command}, function (data) {
+                            if (data.err_text) {
+                                term.error(data.err_text);
+                            } else  if (data.ok_text) {
+                                term.echo("[[i;#E07B08;#000]" + data.ok_text + "]");
+                            }
+                            term.echo("\n[[i;#6773E5;#000]" + data.hint + "]");
+                            updateData(data);
+                            term.set_prompt(prompt);
+                            multilineEnd = false;
+                            commandArray = [];
+                            term.resume();
+                        });
+                    } else {
+                        if (regexp_test) {
+                            multilineEnd = false;
                         }
-                        term.echo("\n[[i;#6773E5;#000]" + data.hint + "]");
-                        updateData(data);
-                        term.resume();
-                    });
+                        commandArray.push(command);
+                        term.set_prompt('... ');
+                    }
+                } else {
+                    term.set_prompt('... ');
+                    if (commandArray.length) {
+                        multilineEnd = true;
+                    }
                 }
         }, {
             prompt: step_prompt,
