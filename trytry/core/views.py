3740c15eea78985a42f9f23d310e3bf791c7400f
Roman Imankulov <roman.imankulov@gmail.com>
2012-08-19 15:09:10 +0600

    Ensure site visitors have CSRF token

--- trytry/core/views.py
+++ trytry/core/views.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from trytry.core.utils import get_all_flows
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 
+@ensure_csrf_cookie
 def index(request):
     flow = request.GET.get('flow', None)
     all_flows = get_all_flows().keys()
